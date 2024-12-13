a352c55edba567e23809adda06a1555cd85993a8
Daniel Brotsky <dan@clickonetwo.io>
2024-12-13 10:29:23 -0800

    Make ListenersView scrollable.
    Fixes #107.

--- whisper/Views/WhisperView/ListenersView.swift
+++ whisper/Views/WhisperView/ListenersView.swift
@@ -12,51 +12,53 @@ struct ListenersView: View {
     @ObservedObject var model: WhisperViewModel
 
     var body: some View {
-		VStack {
-			Text(model.conversation.name)
-				.font(FontSizes.fontFor(FontSizes.minTextSize + 2))
-				.padding()
-			if !model.invites.isEmpty {
-				VStack(alignment: .leading, spacing: 20) {
-					ForEach(model.invites.map(Row.init)) { row in
-						VStack {
-							row.legend
-								.lineLimit(nil)
-								.foregroundColor(colorScheme == .light ? lightPastTextColor : darkPastTextColor)
-							HStack {
-								Button("Accept") { model.acceptRequest(row.id) }
-								Spacer()
-								Button("Refuse") { model.refuseRequest(row.id) }
+		ScrollView {
+			VStack {
+				Text(model.conversation.name)
+					.font(FontSizes.fontFor(FontSizes.minTextSize + 2))
+					.padding()
+				if !model.invites.isEmpty {
+					VStack(alignment: .leading, spacing: 20) {
+						ForEach(model.invites.map(Row.init)) { row in
+							VStack {
+								row.legend
+									.lineLimit(nil)
+									.foregroundColor(colorScheme == .light ? lightPastTextColor : darkPastTextColor)
+								HStack {
+									Button("Accept") { model.acceptRequest(row.id) }
+									Spacer()
+									Button("Refuse") { model.refuseRequest(row.id) }
+								}
+								.buttonStyle(.borderless)
+							}
+						}
+					}
+					Spacer(minLength: 20)
+				}
+				if model.listeners.isEmpty {
+					Text("No Listeners")
+				} else {
+					VStack(alignment: .leading, spacing: 10) {
+						ForEach(model.listeners) { candidate in
+							HStack(spacing: 0) {
+								Text(candidate.info.username)
+									.foregroundColor(colorScheme == .light ? lightPastTextColor : darkPastTextColor)
+								Image(systemName: candidate.remote.kind == .global ? "network" : "personalhotspot")
+									.foregroundColor(colorScheme == .light ? lightPastTextColor : darkPastTextColor)
+								Spacer(minLength: 40)
+								Button(action: { model.shareTranscript(candidate) }, label: { Image(systemName: "eyeglasses") })
+									.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+								Button(action: { model.playSound(candidate) }, label: { Image(systemName: "speaker.wave.2") })
+									.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+								Button(action: { dropListener(candidate) }, label: { Image(systemName: "delete.left") })
 							}
 							.buttonStyle(.borderless)
 						}
 					}
 				}
-				Spacer(minLength: 20)
 			}
-			if model.listeners.isEmpty {
-				Text("No Listeners")
-			} else {
-				VStack(alignment: .leading, spacing: 10) {
-					ForEach(model.listeners) { candidate in
-						HStack(spacing: 0) {
-							Text(candidate.info.username)
-								.foregroundColor(colorScheme == .light ? lightPastTextColor : darkPastTextColor)
-							Image(systemName: candidate.remote.kind == .global ? "network" : "personalhotspot")
-								.foregroundColor(colorScheme == .light ? lightPastTextColor : darkPastTextColor)
-							Spacer(minLength: 40)
-							Button(action: { model.shareTranscript(candidate) }, label: { Image(systemName: "eyeglasses") })
-									.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
-							Button(action: { model.playSound(candidate) }, label: { Image(systemName: "speaker.wave.2") })
-								.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
-							Button(action: { dropListener(candidate) }, label: { Image(systemName: "delete.left") })
-						}
-						.buttonStyle(.borderless)
-					}
-				}
-			}
+			.font(FontSizes.fontFor(FontSizes.minTextSize + 1))
 		}
-		.font(FontSizes.fontFor(FontSizes.minTextSize + 1))
 		.padding()
     }
     
